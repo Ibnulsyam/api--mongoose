400be7f475305962c313ba9fca90f51d36b7651b
ibnul syam <ibnulsyam6@gmail.com>
2022-06-26 15:38:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv").config();
-const port = process.env.DB_PORT || 3000;
-const connectDB = require("./config/mongoose");
+const port = process.env.DB_PORT || 5000;
+const dbConnect = require("./config/mongoose");
 const routerV2 = require("./mongoose/routes");
 
-connectDB();
+dbConnect();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
